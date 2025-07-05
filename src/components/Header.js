40e191e3142f58619de700b9bf3f9481400b027f
eggy-66 <7385nmo@gmail.com>
2025-07-05 11:21:10 +0300

    added buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import '../css/header.css'; // Assuming you have a CSS file for styling
+import '../css/header.css';
+// If using react-i18next:
+import { useTranslation } from 'react-i18next';
 
 function Header() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <header className="header-bg">
       <h1>Eugen Project Posts</h1>
+      <div style={{ marginTop: '1rem' }}>
+        <button onClick={() => changeLanguage('ro')}>Română</button>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('ru')}>Русский</button>
+      </div>
     </header>
   );
 }
